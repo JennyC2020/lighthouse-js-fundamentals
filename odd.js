a773dfcac66bfce8751c6027ba809bd802359119
Jenny Citherlet <jennycith@gmail.com>
2020-06-02 22:40:55 +0000

    deciding wether number is odd or not

---
+++ odd.js
@@ -0,0 +1,13 @@
+
+
+function isOdd(num) {
+  if (num % 2 === 0)
+    return false;
+
+  else 
+    return true;
+    
+}
+console.log(isOdd(4));
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
