1b0222e987fc3c35514c16fac1d8c5662f50fb40
Jenny Citherlet <jennycith@gmail.com>
2020-06-02 22:29:26 +0000

    simplified code

--- loopy.js
+++ loopy.js
@@ -6,7 +6,7 @@ for (let n = 100; n <= 200; n++) {
     console.log("LoopyLighthouse");
   }
   else if (n % 3 === 0){
-    console.log("loopy");
+    console.log("Loopy");
   }
   else if (n % 4 === 0){
     console.log("Lighthouse");
